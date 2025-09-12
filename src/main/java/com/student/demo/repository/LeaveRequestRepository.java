e30398e44617d0cb97b9f8f572b3880e3724f201
selvagApaThY-IT <selvaganapathyn23itpsnacet.edu.in>
2025-09-12 12:19:29 +0530

    new service class

--- src/main/java/com/student/demo/repository/LeaveRequestRepository.java
+++ src/main/java/com/student/demo/repository/LeaveRequestRepository.java
@@ -2,6 +2,7 @@ package com.student.demo.repository;
 
 
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.student.demo.model.LeaveRequest;
@@ -9,6 +10,7 @@ import com.student.demo.model.LeaveRequest;
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
     List<LeaveRequest> findByStatus(String status);
     List<LeaveRequest> findByRollNumber(String rollNumber);
+    void deleteByToDateBefore(LocalDate date);
 
 }
 
---
+++ src/main/java/com/student/demo/service/Leavecleanup.java
@@ -0,0 +1,22 @@
+package com.student.demo.service;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.student.demo.repository.LeaveRequestRepository;
+
+@Service
+public class Leavecleanup {
+	@Autowired
+	LeaveRequestRepository leaverepo;
+	 @Transactional
+	@Scheduled(cron="0 0 0 * * ?")
+	public void deletereqbydatend() {
+		LocalDate today1=LocalDate.now();
+		leaverepo.deleteByToDateBefore(today1);
+	}
+}
